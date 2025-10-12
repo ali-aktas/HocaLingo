a6d949833a9a8697c358dfd8c295599843800dfe
Ali Aktaş <aliaktasofficial@gmail.com>
2025-10-12 23:04:13 +0300

    Before macbook

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.hocalingo.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 5
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
